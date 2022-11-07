0934831286a293863b1ae69dc48e6b502700230a
BachBox <101316321+BachBox@users.noreply.github.com>
2022-11-07 17:50:35 +0700

    Add files via upload

---
+++ TheGreatRobberyApp.java
@@ -0,0 +1,15 @@
+
+public class TheGreatRobberyApp {
+    public static void main(String[] args) throws Exception {
+        City city = new City();
+        Gang theGang = new Gang();
+        Police police = new Police();
+        theGang.printGangInfo();
+        police.getSherlockHolmes().printBioData();
+        
+        System.out.println("================");
+        do {
+            theGang.letsRob(city.getBuildings());
+        } while (!police.catchCriminals(theGang));
+    }
+}
